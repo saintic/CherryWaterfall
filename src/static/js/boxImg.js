c8d1e742b21a9da8b72d99a4e18f1fa6207f2f78
Mr.tao <staugur@vip.qq.com>
2018-06-05 21:45:20 +0800

    update样式

--- src/static/js/boxImg.js
+++ src/static/js/boxImg.js
@@ -13,13 +13,13 @@ $("img[modal='zoomImg']").each(function () {
             "   <div class=\"mask-layer-black\"></div>" +
             "   <div class=\"mask-layer-container\">" +
             "       <div class=\"mask-layer-container-operate\">" +
-            "           <button class=\"mask-prev btn-default-styles\" style=\"float: left\">上一张</button>" +
+            "           <button class=\"mask-prev btn-default-styles\">上一张</button>" +
             "           <button class=\"mask-out btn-default-styles\">放大</button>" +
             "           <button class=\"mask-in btn-default-styles\">缩小</button>" +
             "           <button class=\"mask-clockwise btn-default-styles\">顺旋转</button>" +
             "           <button class=\"mask-counterclockwise btn-default-styles\">逆旋转</button>" +
             "           <button class=\"mask-close btn-default-styles\">关闭</button>" +
-            "           <button class=\"mask-next btn-default-styles\" style=\"float: right\">下一张</button>" +
+            "           <button class=\"mask-next btn-default-styles\">下一张</button>" +
             "       </div>" +
             "       <div class=\"mask-layer-imgbox auto-img-center\"></div>" +
             "   </div>" +
